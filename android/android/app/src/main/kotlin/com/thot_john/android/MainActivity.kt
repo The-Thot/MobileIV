34de7eca77eda22cd67dc00c964910c54b8e9ec8
giovamev <110948779+giovamev@users.noreply.github.com>
2022-10-28 19:43:37 -0500

    Delete android directory
    cargue de archivo completo

--- android/android/app/src/main/kotlin/com/thot_john/android/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.thot_john.android
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- android/app/src/main/kotlin/com/thot_john/john_thot/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.thot_john.john_thot
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
